374418379936649905f4aa6ef450db0b336303a0
Emapiz <84033336+Emapiz@users.noreply.github.com>
2023-07-04 02:29:02 +0200

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Univaq',
+  appName: 'Geo-Piece',
   webDir: 'www',
   server: {
     androidScheme: 'https'
